fc742bcfb42526e6fef83d247ec514bd26fc4e12
Aditya <adityak00927@gmail.com>
2025-02-22 22:11:43 +0530

    Project Added

---
+++ random-color-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import RandomColor from './components/random-color'
+
+function App() {
+
+
+  return (
+    <>
+     {/* Random Color Component */}
+     <RandomColor />
+    </>
+  )
+}
+
+export default App
